d63843805bcce98854e6b6079a043cf09cfb4c0e
danif <danifarre97@gmail.com>
2021-01-10 13:32:53 +0100

    Integration

--- src/common/ProfessorServer.java
+++ src/common/ProfessorServer.java
@@ -2,10 +2,11 @@ package common;
 
 import exam.Question;
 
+import java.io.IOException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ProfessorServer extends Remote {
-    void registerStudent(StudentClient clien, String studentId) throws RemoteException;
+    void registerStudent(StudentClient clien, String studentId) throws IOException;
     void sendAnswer(String studentId, Question question) throws RemoteException;
 }
